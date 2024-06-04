0b2284e9bb4fc382da04f2da7f850a0f03888007
yurlev911 <134205200+yurlev911@users.noreply.github.com>
2024-06-04 12:11:58 +0900

    Add files via upload

---
+++ Lab20Server/src/Client.java
@@ -0,0 +1,150 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client extends JFrame {
+    private final String SERVER_ADDR = "localhost";
+    private final int SERVER_PORT = 8189;
+    private JTextField msgInputField;
+    private JTextArea chatArea;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    public Client() {
+        try {
+            openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        prepareGUI();
+    }
+
+    public void openConnection() throws IOException {
+        socket = new Socket(SERVER_ADDR, SERVER_PORT);
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
+
+        new Thread(new Runnable() {
+            @Override
+            public void run(){
+                try {
+                    while (true){
+                        String strFromServer = in.readUTF();
+                        if (strFromServer.startsWith("/authok")){
+                            break;
+                        }
+                        chatArea.append(strFromServer + "\n");
+                    }
+                    while (true){
+                        String strFromServer = in.readUTF();
+                        if (strFromServer.equalsIgnoreCase("/end")){
+                            break;
+                        }
+                        chatArea.append(strFromServer);
+                        chatArea.append("\n");
+                    }
+                }
+                catch (EOFException eofException){}
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    public void closeConnection() {
+        try {
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessage() {
+        if (!msgInputField.getText().trim().isEmpty()) {
+            try {
+                out.writeUTF(msgInputField.getText());
+                msgInputField.setText("");
+                msgInputField.grabFocus();
+            } catch (IOException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Ошибка отправки сообщения");
+            }
+        }
+    }
+
+    public void prepareGUI() {
+        // Параметры окна
+        setBounds(600, 300, 500, 500);
+        setTitle("Клиент");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        // Текстовое поле для вывода сообщений
+        chatArea = new JTextArea();
+        chatArea.setEditable(false);
+        chatArea.setLineWrap(true);
+        add(new JScrollPane(chatArea), BorderLayout.CENTER);
+        // Нижняя панель с полем для ввода сообщений и кнопкой отправки сообщений
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        JButton btnSendMsg = new JButton("Отправить");
+        bottomPanel.add(btnSendMsg, BorderLayout.EAST);
+        msgInputField = new JTextField();
+        add(bottomPanel, BorderLayout.SOUTH);
+        bottomPanel.add(msgInputField, BorderLayout.CENTER);
+        btnSendMsg.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sendMessage();
+            }
+        });
+        msgInputField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sendMessage();
+            }
+        });
+        // Настраиваем действие на закрытие окна
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                try {
+                    out.writeUTF("/end");
+                    closeConnection();
+                } catch (IOException exc) {
+                    exc.printStackTrace();
+                }
+            }
+        });
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Client();
+            }
+        });
+    }
+
+
+}
